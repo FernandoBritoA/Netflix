0d1524310b57eace370a9dffb9d30f4557dd15a1
Fernando Brito <fernando_britoa@hotmail.com>
2023-08-08 22:20:49 -0600

    Added navigation on press item across ciew controllers

--- Netflix/Controllers/Core/SearchViewController.swift
+++ Netflix/Controllers/Core/SearchViewController.swift
@@ -96,6 +96,36 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        guard let titleName = title.original_title ?? title.original_name else {return}
+        
+        ApiCaller.shared.getMovieTrailer(with: titleName) { [weak self] results in
+            switch results {
+            case .success(let movie):
+                guard let safeSelf = self else {
+                    return
+                }
+                
+                let viewModel = TitlePreviewViewModel(
+                    title: titleName,
+                    youtubeVideoId: movie.id.videoId ?? "",
+                    titleOverview: title.overview ?? "")
+                
+                DispatchQueue.main.async {
+                    let vc = TitlePreviewViewController()
+                    vc.configure(with: viewModel)
+                    safeSelf.navigationController?.pushViewController(vc, animated: true)
+                }
+
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
 extension SearchViewController: UISearchResultsUpdating {
